4fc1bcdc39a09696f114d5c4ec0c6b1ea8d5788d
Marc <kemar79@qq.com>
2025-09-28 12:46:11 +0100

    navigate to starters activty and back to main activity #4

--- app/src/main/java/com/marcal/grabaspoon/MainActivity.java
+++ app/src/main/java/com/marcal/grabaspoon/MainActivity.java
@@ -1,6 +1,8 @@
 package com.marcal.grabaspoon;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
 
         startersCard = findViewById(R.id.card_view_starters);
         mainsCard = findViewById(R.id.card_view_main);
+
+        startersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startersActivityIntent = new Intent(MainActivity.this,
+                        StartersActivity.class);
+                startActivity(startersActivityIntent);
+            }
+        });
     }
 }
